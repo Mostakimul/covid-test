b136c709c460520d9562edf75410ed94e521653a
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-17 16:14:37 +0200

    Home Page Structure markedup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <h2>Number: {{ number }}</h2>
+  <div class="grid grid-cols-4 gap-5">
+    <div class="col-span-full md:col-span-3">
+      <div class="bg-white text-center mb-5 rounded shadow-lg py-5">
+        <h3>All Statictics</h3>
+      </div>
+    </div>
+    <div class="col-span-full md:col-span-1">
+      <div class="bg-white text-center mb-5 rounded shadow-lg py-5">
+        <h3>Please Choose a Country</h3>
+        <div class="p-5">
+      <select
+        class="bg-gray-300 py-2 px-3 rounded"
+      >
+        <option disabled value>Please select a country</option>
+        <option>Choices</option>
+      </select>
+    </div>
+      </div>
+    </div>
   </div>
 </template>
 
