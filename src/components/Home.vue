755ad3d1c54dad563e0fef172665ac041b5d0764
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-17 16:28:23 +0200

    showing all country in option

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,36 +8,37 @@
     <div class="col-span-full md:col-span-1">
       <div class="bg-white text-center mb-5 rounded shadow-lg py-5">
         <h3>Please Choose a Country</h3>
-        <div class="p-5">
-      <select
-        class="bg-gray-300 py-2 px-3 rounded"
-      >
-        <option disabled value>Please select a country</option>
-        <option>Choices</option>
-      </select>
-    </div>
+        <div class="p-2">
+          <select v-model="selectedCountry" class="bg-gray-300 py-2 px-1 rounded">
+            <option disabled value>Please select a country</option>
+            <option
+              v-for="country in allCountries"
+              :key="country.name"
+              :value="country.name"
+            >{{ country.name }}</option>
+          </select>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        baseURL: 'http://localhost:8080/api',
-        allCountries: [],
-        selectedCountry: ''
-      }
-    },
-    async mounted () {
-      const res = await fetch(`${this.baseURL}/regions`)
-      const data = await res.json()
-      console.log(data.data);
-    },
-  }
+export default {
+  data() {
+    return {
+      baseURL: 'http://localhost:8080/api',
+      allCountries: [],
+      selectedCountry: ''
+    }
+  },
+  async mounted() {
+    const res = await fetch(`${this.baseURL}/regions`)
+    const data = await res.json()
+    this.allCountries = data.data;
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
